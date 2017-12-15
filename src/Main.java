739215136371f6ae88ab96e2bd5c0d7509c8185e
mathonet <romain.mathonet@student.ulg.ac.be>
2017-12-15 15:27:47 +0100

    Classes

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ISolver;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("coucou");
+        ISolver solver = SolverFactory.newDefault();
+
+
+
+    }
+}
+
--- src/Solver.java
+++ src/Solver.java
@@ -3,12 +3,5 @@ import org.sat4j.specs.ISolver;
 
 public class Solver {
 
-    public static void main(String[] args)
-    {
-        System.out.println("coucou");
-        ISolver solver = SolverFactory.newDefault();
 
-
-
-    }
 }

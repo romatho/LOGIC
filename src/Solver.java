50a73c411be4fcaebfc7cd9df456d0c01053e94c
mathonet <romain.mathonet@student.ulg.ac.be>
2017-12-15 13:43:28 +0100

    bitch

--- src/Solver.java
+++ src/Solver.java
@@ -1,10 +1,12 @@
-import org.sat4j.*;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ISolver;
 
 public class Solver {
 
-    public void main()
+    public static void main(String[] args)
     {
         System.out.println("coucou");
+        ISolver solver = SolverFactory.newDefault();
 
 
 

0c3c8a5fced8c404a4b748447d0bed9136895c4f
mathonet <romain.mathonet@student.ulg.ac.be>
2017-12-16 11:49:47 +0100

    ARTHOUUUUUUR

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,2 +1,20 @@
+import java.io.*;
+
 public class FileHandler {
+
+    public FileHandler(String Filename)
+    {
+        File file = new File(Filename);
+        FileReader fileReader ;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+
+        }
+
+    }
+
+
 }

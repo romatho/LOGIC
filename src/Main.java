014ee1bb3ee60786537acdcd3eba0295c8395d29
mathonet <romain.mathonet@student.ulg.ac.be>
2017-12-18 22:33:51 +0100

    ARTHOUUUUUUR

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,14 @@ import org.sat4j.specs.TimeoutException;
 
 /* TODO list (j'ai pas trouvé comment ajouter un fichier texte et j'avais peur de tout casser
 *
-* - vérifier que le code fonctionne sur window
-* - arranger le code du main pour avoir un truc propre en fonction du nombre d'argument au lieu d'un IF ELSE moche
-* - recréer des fonction dans la classe Main pour avoir une méthode main plus belle que cette horreur
-* - améliorer le main pour récupérer les argument au lieu de hard coder les chemins des fichiers
+* - vérifier que le code fonctionne sur MS800
+* - arranger le code du main pour avoir un truc propre en fonction du nombre d'argument au lieu d'un IF ELSE moche DONE
+* - améliorer le main pour récupérer les argument au lieu de hard coder les chemins des fichiers DONE
 * - commenter le code
 * - faire le rapport
 * - fournir un build.xml
 * - forunir un INSTALL file si nécessaire
-*
+*- outputer le sudoku résolu
 *
 *
 *
@@ -38,10 +37,9 @@ public class Main {
     public static void main(String[] args)
     {
         int size = 9;
-        boolean mode = false;
         FileHandler fileHandler = new FileHandler();
-        if(mode) {
-            fileHandler.readSudoku("C:\\Users\\kidre\\sudoku2.in");
+        if(args.length==2) {
+            fileHandler.readSudoku(args[0]);
             Grid grid = new Grid(fileHandler.getSudoku());
             grid.display();
             try {
@@ -52,6 +50,7 @@ public class Main {
                 if (problem.isSatisfiable()) {
                     System.out.println(" The solution is:");
                     int[] model = solver.model();
+
                     for (int l = 0; l < model.length; l++) {
                         if (l % 81 == 0)
                             System.out.println();
@@ -72,16 +71,22 @@ public class Main {
                 System.out.println(" Timeout , sorry !");
             }
         }
-        else
+        else if(args.length==1)
         {
             Grid grid = new Grid(new int[size][size]);
             ISolver solver = SolverFactory.newDefault();
             solver.newVar(size * size * size);
             grid.addClause(solver);
             grid.fillSudoku(solver);
-            fileHandler.writeSudoku("C:\\Users\\kidre\\sudoku3.txt",grid.getSudoku(), grid.getSize());
+            fileHandler.writeSudoku(args[0],grid.getSudoku(), grid.getSize());
             grid.display();
         }
+        else
+            {
+                System.out.println(" Wrong number of arguments. If you want to:\n" +
+                        " generate a sudoku you should enter the output file as argument\n " +
+                        " sovle a sudoku you shoudl enter the imput file followed by the output file");
+            }
     }
 }
 
